2b4d8d35f69e07ff926bb51129edb55631e1890d
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-03-31 17:33:18 +0100

    added a form in AddRecipe Component so the user can add a recipe

--- recipr/src/pages/addRecipe.js
+++ recipr/src/pages/addRecipe.js
@@ -4,6 +4,28 @@ const AddRecipe = () => {
 	return (
 		<div>
 			<h1>Add Recipe!</h1>
+			<form method="POST">
+				<input
+					placeholder='Recipe title'
+					type='text'
+				/>
+				<input
+					placeholder='Add Image'
+					type='text'
+				/>
+				<input
+					placeholder='Add Description...'
+					type='text'
+				/>
+				<input
+					placeholder='Add Ingredients...'
+					type='text'
+				/>
+				<button
+				>
+					Submit Recipe
+				</button>
+			</form>
 		</div>
 	)
 }
