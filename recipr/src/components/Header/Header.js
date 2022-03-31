df8aa1148b76f952d494d37365b96236bc7c3376
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-03-31 17:22:33 +0100

    added react-router npm package and set up corresponding paths

---
+++ recipr/src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./Header.css"
+
+function Header() {
+	return (
+		<nav>
+			<div className='logo'>
+				<h1>Recipr</h1>
+				<img src="/images/logo.png" alt='blog-logo' className='logo__img'></img>
+			</div>
+			<div className='logo__navlinks'>
+				<Link to="/">
+					<p>My Recipes</p>
+				</Link>
+				<Link to="/about">
+					<p>About</p>
+				</Link>
+				<Link to="/contact-us">
+					<p>Contact Us</p>
+				</Link>
+
+			</div>
+
+		</nav>
+	)
+}
+
+export default Header
