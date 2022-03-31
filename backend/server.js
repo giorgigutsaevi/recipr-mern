930296b26c4e7319867a9b3b279adc2079f41b5a
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-03-31 16:28:43 +0100

    setup connection to mongoDB with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 import express from "express"
 import Cors from "cors"
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 
 dotenv.config();
+
+const uri = process.env.RECIPR_DB_URI;
+mongoose.connect(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
 
 // configuring the server
 const app = express();
@@ -12,7 +19,8 @@ const port = 5001;
 app.use(express.json());
 app.use(Cors());
 
-app.get("/", (req, res) => {
+// API ENDPOINT configuration
+app.get("/api/v1/recipes", (req, res) => {
 	res.status(200).send("Homepage")
 })
 
