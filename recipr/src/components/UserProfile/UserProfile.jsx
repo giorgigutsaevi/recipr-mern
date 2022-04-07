d48a19c21d62b180c511ce937e9932faab241ef9
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-04-07 15:14:41 +0100

    added styling to UserProfile component and a function to display current date

--- recipr/src/components/UserProfile/UserProfile.jsx
+++ recipr/src/components/UserProfile/UserProfile.jsx
@@ -3,24 +3,37 @@ import React, { useContext, useState, useEffect } from "react";
 import UserContext from "../../Context/UserContext";
 import jwtDecode from "jwt-decode";
 import "./UserProfile.css";
+import { toDate } from "date-fns/esm";
 
 const UserProfile = () => {
   const [username, setUsername] = useState("");
   const { userToken } = useContext(UserContext);
-	console.log("FROM USER PROFILE -----> ", userToken)
 
+  const displayDate = () => {
+    const currentDate = new Date();
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    };
+    return currentDate.toLocaleDateString("en-us", options);
+  };
 
   useEffect(() => {
     if (userToken) {
       const decoded = jwtDecode(userToken);
-      const decodedUsername = decoded.username
-			setUsername(decodedUsername)
+      const decodedUsername = decoded.username;
+      setUsername(decodedUsername);
     }
   }, []);
 
   return (
-    <div>
-      <h1>Welcome, {username}</h1>
+    <div className="userProfile">
+      <div className='userProfile--message__wrapper'>
+        <h1 className="userProfile__title">Welcome, {username} 👋 </h1>
+        <h5 className="userProfile__date">{displayDate()}</h5>
+      </div>
     </div>
   );
 };
