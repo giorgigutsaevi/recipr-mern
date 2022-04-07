8342c0ab6d6d3f119b62e41cf811ee86251157f3
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-04-07 13:02:34 +0100

    installed jtwdecoder npm package to decode my jtw token and extract user's information for rendering

--- recipr/src/components/UserProfile/UserProfile.jsx
+++ recipr/src/components/UserProfile/UserProfile.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useContext } from "react";
+import UserContext from "../../Context/UserContext";
+import jwtDecoder from "jwt-decode";
+import "./UserProfile.css";
 
 const UserProfile = () => {
-	return (
-		<div>UserProfile</div>
-	)
-}
+  const { userToken, setUserToken } = useContext(UserContext);
+  
+  var token = userToken;
+  var decoded = jwtDecoder(token);
+
+  return <div></div>;
+};
 
 export default UserProfile;
